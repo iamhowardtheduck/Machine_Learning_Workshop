0a52748f5765cd14aee45aced3e1c8b03f16e85e
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-04-16 12:33:50 -0400

    Update livedata.sh

--- Scripts/livedata.sh
+++ Scripts/livedata.sh
@@ -1,8 +1,8 @@
-python /workspace/workshop/Machine-Learning-Workshop/run_workshop.py \
+python /workspace/workshop/Machine_Learning_Workshop/run_workshop.py \
   --host http://kubernetes-vm:30920 \
   --user sdg --password changeme \
   --no-verify-ssl \
-  --sdg-config /workspace/workshop/Machine-Learning-Workshop/mortgage-workshop.yml \
+  --sdg-config /workspace/workshop/Machine_Learning_Workshop/mortgage-workshop.yml \
   --anomaly-chance 0.07 \
   --status-interval 3
   
