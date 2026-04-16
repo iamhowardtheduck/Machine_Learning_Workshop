845ea29669728df7f089ac6e18496e3b9a1ab38a
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-04-16 09:43:49 -0400

    Create livedata.sh

---
+++ Scripts/livedata.sh
@@ -0,0 +1,10 @@
+python /workspace/workshop/Machine-Learning-Workshop/run_workshop.py \
+  --host http://kubernetes-vm:30920 \
+  --user sdg --password changeme \
+  --no-verify-ssl \
+  --sdg-config /workspace/workshop/Machine-Learning-Workshop/mortgage-workshop.yml \
+  --anomaly-chance 0.07 \
+  --status-interval 3
+  
+  
+  
