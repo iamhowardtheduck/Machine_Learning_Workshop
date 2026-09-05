a6064760aa7e3db45ee57b9734b88f8bc347b57d
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-09-05 17:34:53 -0400

    Create backfill.sh

---
+++ Scripts/V2/backfill.sh
@@ -0,0 +1,11 @@
+python3  /workspace/workshop/Machine_Learning_Workshop/backfill_all-MLv2-WORKSHOP.py \
+        --host https://es.elastic.lab:443 \
+        --user sdg --password changeme \
+        --days 30 \
+        --no-verify-ssl \
+        --sdg-bulk 7500  \
+        --apm-bulk 2500 \
+        --no-then-run --timezone "UTC" \
+        --spike-count 3 --spike-volume-mult 2 --spike-duration-hrs 1 \
+        --spike-error-rate 0.3 --spike-latency-mult 2 --spike-cap-override \
+        --skip-ml
