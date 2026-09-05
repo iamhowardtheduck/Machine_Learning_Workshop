c3cdab70c1e52c11cb3a477e1acb7b9ea82e5c7d
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-09-05 17:36:06 -0400

    Create bootstrap.sh

---
+++ Scripts/V2/bootstrap.sh
@@ -0,0 +1,8 @@
+pip install faker elasticsearch --break-system-packages 
+python /workspace/workshop/Machine_Learning_Workshop/bootstrap.py \
+  --host http://es.elastic.lab:443 \
+  --user sdg \
+  --password changeme \
+  --kibana-host http://kb.elastic.lab:443 \
+  --no-verify-ssl \
+  --skip-ml
