b89332b0356d24d83d0c3a97aac32e79a515404d
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-09-06 19:44:19 -0400

    Update backfill.sh

--- Scripts/V2/backfill.sh
+++ Scripts/V2/backfill.sh
@@ -1,3 +1,6 @@
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install elasticsearch faker pyyaml
 python3  /home/elastic/Machine_Learning_Workshop/backfill_all-MLv2-WORKSHOP.py \
         --host https://es.elastic.lab:443 \
         --user sdg --password changeme \
