1e7af01d3fb462847d7e40545214ffec938b0c4f
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-09-05 21:36:15 -0400

    Update livedata.sh

--- Scripts/V2/livedata.sh
+++ Scripts/V2/livedata.sh
@@ -2,6 +2,6 @@ python /home/elastic/Machine_Learning_Workshop/run_workshop.py \
   --host https://es.elastic.lab:443 \
   --user sdg --password changeme \
   --no-verify-ssl \
-  --sdg-config /workspace/workshop/Machine_Learning_Workshop/mortgage-workshop.yml \
+  --sdg-config /home/elastic/Machine_Learning_Workshop/mortgage-workshop.yml \
   --anomaly-chance 0.07 \
   --status-interval 3
