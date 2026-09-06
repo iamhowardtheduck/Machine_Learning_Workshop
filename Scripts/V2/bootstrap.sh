9b3b2184e968397c4a32762f189d5fddb85b581c
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-09-06 12:14:49 -0400

    Update bootstrap.sh

--- Scripts/V2/bootstrap.sh
+++ Scripts/V2/bootstrap.sh
@@ -1,8 +1,8 @@
 pip install faker elasticsearch --break-system-packages 
-python /home/elastic/Machine_Learning_Workshop/bootstrap.py \
-  --host http://es.elastic.lab:443 \
+python3 /home/elastic/Machine_Learning_Workshop/bootstrap.py \
+  --host https://es.elastic.lab:443 \
   --user sdg \
   --password changeme \
-  --kibana-host http://kb.elastic.lab:443 \
+  --kibana-host https://kb.elastic.lab:443 \
   --no-verify-ssl \
   --skip-ml
