9759a169f70c8d292542021f11847a6d0254b007
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-09-06 11:59:28 -0400

    Update cluster-config.sh

--- Scripts/V2/cluster-config.sh
+++ Scripts/V2/cluster-config.sh
@@ -2,7 +2,7 @@
 echo 'ELASTICSEARCH_USERNAME=elastic' >> /home/elastic/Machine_Learning_Workshop/.env
 #echo -n 'ELASTICSEARCH_PASSWORD=' >> /root/.env
 kubectl get secret elasticsearch-es-elastic-user -n default -o go-template='ELASTICSEARCH_PASSWORD={{.data.elastic | base64decode}}' >> /home/elastic/Machine_Learning_Workshop/.env
-echo '' >> /root/.env
+echo '' >> /home/elastic/Machine_Learning_Workshop/.env
 echo 'ELASTICSEARCH_URL="https://es.elastic.lab:443"' >> /home/elastic/Machine_Learning_Workshop/.env
 echo 'KIBANA_URL="https://kb.elastic.lab:443"' >> /home/elastic/Machine_Learning_Workshop/.env
 echo 'BUILD_NUMBER="10"' >> /home/elastic/Machine_Learning_Workshop/.env
