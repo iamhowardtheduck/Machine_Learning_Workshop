2acd056b511dccbf48f2d06781e438281abc8683
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-09-06 13:44:17 -0400

    Update bootstrap.sh

--- Scripts/V2/bootstrap.sh
+++ Scripts/V2/bootstrap.sh
@@ -1,4 +1,7 @@
-pip install faker elasticsearch --break-system-packages 
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install elasticsearch faker pyyaml
+#pip install faker elasticsearch --break-system-packages 
 python3 /home/elastic/Machine_Learning_Workshop/bootstrap.py \
   --host https://es.elastic.lab:443 \
   --user sdg \
